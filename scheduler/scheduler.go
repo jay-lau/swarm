b06ca1e5adec8d28c0d76838e14e03099d2320b1
Guang Ya Liu <liugya@cn.ibm.com>
2015-06-25 13:52:57 +0800

    update

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,52 +1,76 @@
 package scheduler
 
 import (
-	"strings"
-	"sync"
+        "strings"
+        "sync"
+        "unsafe"
 
-	"github.com/docker/swarm/cluster"
-	"github.com/docker/swarm/scheduler/filter"
-	"github.com/docker/swarm/scheduler/node"
-	"github.com/docker/swarm/scheduler/strategy"
+        "github.com/docker/swarm/cluster"
+        "github.com/docker/swarm/scheduler/filter"
+        "github.com/docker/swarm/scheduler/node"
+        "github.com/docker/swarm/scheduler/strategy"
+        log "github.com/Sirupsen/logrus"
+)
+
+const (
+        pkg   = "scheduler"
+        class = "Scheduler"
 )
 
 // Scheduler is exported
 type Scheduler struct {
-	sync.Mutex
+        sync.Mutex
 
-	strategy strategy.PlacementStrategy
-	filters  []filter.Filter
+        strategy strategy.PlacementStrategy
+        filters  []filter.Filter
 }
 
 // New is exported
 func New(strategy strategy.PlacementStrategy, filters []filter.Filter) *Scheduler {
-	return &Scheduler{
-		strategy: strategy,
-		filters:  filters,
-	}
+        return &Scheduler{
+                strategy: strategy,
+                filters:  filters,
+        }
 }
 
 // SelectNodeForContainer will find a nice home for our container.
 func (s *Scheduler) SelectNodeForContainer(nodes []*node.Node, config *cluster.ContainerConfig) (*node.Node, error) {
-	accepted, err := filter.ApplyFilters(s.filters, config, nodes)
-	if err != nil {
-		return nil, err
-	}
+        accepted, err := filter.ApplyFilters(s.filters, config, nodes)
+        if err != nil {
+                return nil, err
+        }
 
-	return s.strategy.PlaceContainer(config, accepted)
+        return s.strategy.PlaceContainer(config, accepted)
+}
+
+// RemoveAllocation will removed allocation from the scheduler
+func (s *Scheduler) RemoveAllocation(id string) error {
+        var fn = "RemoveAllocation"
+        log.Debugf("[%s/%s/%s] Entering...",  pkg, class, fn)
+        var name = s.strategy.Name()
+        if name == "ego" {
+                var strat strategy.PlacementStrategy = s.strategy
+                pstrat := (*strategy.EGOPlacementStrategy) (unsafe.Pointer(&strat))
+                err := pstrat.ReleaseAllocation(id)
+                if err == nil {
+                        log.Debugf("[%s/%s/%s] Removed allocation id %s from scheduler.",  pkg, class, fn, id)
+                }
+        }
+        return nil
 }
 
 // Strategy returns the strategy name
 func (s *Scheduler) Strategy() string {
-	return s.strategy.Name()
+        return s.strategy.Name()
 }
 
 // Filters returns the list of filter's name
 func (s *Scheduler) Filters() string {
-	filters := []string{}
-	for _, f := range s.filters {
-		filters = append(filters, f.Name())
-	}
+        filters := []string{}
+        for _, f := range s.filters {
+                filters = append(filters, f.Name())
+        }
 
-	return strings.Join(filters, ", ")
+        return strings.Join(filters, ", ")
 }
+
